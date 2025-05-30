b3b4e044eed0f18a7693a1c1d882beae9406cab7
Diamond-Gabie-99 <umohdiamond807@gmail.com>
2025-05-30 15:01:20 +0100

    f hj

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser, BookingReport, RevenueReport, UserGrowthReport, TourPerformanceReport
+from .models import CustomUser, BookingReport, RevenueReport, UserGrowthReport, TourPerformanceRepor
 
 
 # Custom User Admin
